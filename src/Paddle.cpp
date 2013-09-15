fe15327c519b6e3a5c16587209d253318b283a05
Adrian Wagner <aw@commit.github>
2013-09-15 17:29:26 +0200

    Fixed paddle shape
    - behaves the same, but displays right

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -3,12 +3,13 @@
 Paddle::Paddle(shared_ptr<EntityManager> manager, int index, pair<sf::Vector2f, sf::Vector2f> outerEdge, sf::Vector2f midScreen, float length):
 Entity(manager, EntityType::EPaddle), index_(index)
 {
-	shared_ptr<sf::ConvexShape> shape=make_shared<sf::ConvexShape>(3);
-	shape->setPoint(0, midScreen);
+	shared_ptr<sf::ConvexShape> shape=make_shared<sf::ConvexShape>(4);
+	shape->setPoint(0, outerEdge.second);
+	shape->setPoint(1, outerEdge.first);
 	sf::Vector2f firstToMid=Math::normalize(outerEdge.first-midScreen);
-	shape->setPoint(1, outerEdge.first+firstToMid*length);
+	shape->setPoint(2, outerEdge.first+firstToMid*length);
 	sf::Vector2f secondToMid=Math::normalize(outerEdge.second-midScreen);
-	shape->setPoint(2, outerEdge.second+secondToMid*length);
+	shape->setPoint(3, outerEdge.second+secondToMid*length);
 	shape->setOrigin(midScreen);
 	shape->setPosition(midScreen);
 	shape->setFillColor(sf::Color(37, 165, 255));
